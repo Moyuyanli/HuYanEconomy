0b3a361a7f8a247b745e5d2585143df573538240
Moyuyanli <572490972@qq.com>
2024-08-11 16:47:12 +0800

    feat:添加颜色转换工具

--- src/main/java/cn/chahuyun/economy/utils/ImageUtil.java
+++ src/main/java/cn/chahuyun/economy/utils/ImageUtil.java
@@ -155,6 +155,7 @@ public class ImageUtil {
 
     /**
      * 十六进制颜色
+     *
      * @param color 颜色
      * @return Color
      */
@@ -168,4 +169,28 @@ public class ImageUtil {
         }
         throw new IllegalArgumentException("构建颜色错误!");
     }
+
+
+    /**
+     * 将color换成16进制的颜色，不带#
+     *
+     * @param color 颜色
+     * @return 16进制的颜色
+     */
+    public static String colorHex(Color color) {
+        // 将颜色的红、绿、蓝分量转换为16进制字符串
+        String red = Integer.toHexString(color.getRed());
+        String green = Integer.toHexString(color.getGreen());
+        String blue = Integer.toHexString(color.getBlue());
+
+
+        // 补齐两位数
+        if (red.length() == 1) red = "0" + red;
+        if (green.length() == 1) green = "0" + green;
+        if (blue.length() == 1) blue = "0" + blue;
+
+        // 返回格式化的16进制颜色字符串
+        return red + green + blue;
+    }
+
 }
