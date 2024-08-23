efaebc5e63c326c2047074f420cc115a4616bc37
Moyuyanli <572490972@qq.com>
2024-08-23 20:41:26 +0800

    feat:添加从群消息获取at群成员的工具方法

--- src/main/java/cn/chahuyun/economy/utils/ShareUtils.java
+++ src/main/java/cn/chahuyun/economy/utils/ShareUtils.java
@@ -2,13 +2,20 @@ package cn.chahuyun.economy.utils;
 
 import cn.chahuyun.economy.HuYanEconomy;
 import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.contact.Member;
 import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.event.GlobalEventChannel;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.event.events.MessageEvent;
+import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.MessageChain;
+import net.mamoe.mirai.message.data.SingleMessage;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 公共工具
@@ -55,9 +62,9 @@ public class ShareUtils {
     /**
      * 替换字符串中的变量
      *
-     * @param s 原始字符串
+     * @param s        原始字符串
      * @param variable 变量数组
-     * @param content 替换内容数组
+     * @param content  替换内容数组
      * @return 替换后的字符串
      */
     public static String replacer(String s, String[] variable, Object... content) {
@@ -67,5 +74,26 @@ public class ShareUtils {
         return s;
     }
 
+    /**
+     * 获取消息中的at人<p/>
+     * 包括成功at消息和[@xxx]消息
+     *
+     * @param event 群消息事件
+     * @return at群成员，可能为空
+     */
+    public static Member getAtMember(GroupMessageEvent event) {
+        MessageChain message = event.getMessage();
+        for (SingleMessage singleMessage : message) {
+            if (singleMessage instanceof At) {
+                return event.getGroup().get(((At) singleMessage).getTarget());
+            }
+        }
+        String content = message.contentToString();
+        Matcher matcher = Pattern.compile("@\\d{5,10}").matcher(content);
+        if (matcher.find()) {
+            return event.getGroup().get(Long.parseLong(matcher.group().substring(1)));
+        }
+        return null;
+    }
 
 }
