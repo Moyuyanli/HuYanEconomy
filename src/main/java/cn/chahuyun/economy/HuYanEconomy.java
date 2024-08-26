023acad03afdf37abe1613a5e7f9493c41f38e5f
Moyuyanli <RoxyGreerad@163.com>
2024-08-26 14:52:34 +0800

    release:1.4.4

--- src/main/java/cn/chahuyun/economy/HuYanEconomy.java
+++ src/main/java/cn/chahuyun/economy/HuYanEconomy.java
@@ -35,7 +35,7 @@ public final class HuYanEconomy extends JavaPlugin {
     /**
      * 全局版本
      */
-    public static final String VERSION = "1.4.3";
+    public static final String VERSION = "1.4.4";
     /**
      * 配置
      */
