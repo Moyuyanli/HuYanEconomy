238367308ee7eadfd22bf432088da79198b649b3
Moyuyanli <RoxyGreerad@163.com>
2024-08-27 14:00:39 +0800

    release:1.4.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "cn.chahuyun"
-version = "1.4.5"
+version = "1.4.6"
 
 repositories {
     mavenCentral()
--- src/main/java/cn/chahuyun/economy/HuYanEconomy.java
+++ src/main/java/cn/chahuyun/economy/HuYanEconomy.java
@@ -33,7 +33,7 @@ public final class HuYanEconomy extends JavaPlugin {
     /**
      * 全局版本
      */
-    public static final String VERSION = "1.4.5";
+    public static final String VERSION = "1.4.6";
     /**
      * 配置
      */
