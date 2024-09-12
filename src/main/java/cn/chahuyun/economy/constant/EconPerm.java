b16c129442dc3b987c124e1496659ad737f59d8c
moyuyanli <572490972@qq.com>
2024-09-12 15:08:50 +0800

    fix:替换权限名称

---
+++ src/main/java/cn/chahuyun/economy/constant/EconPerm.java
@@ -0,0 +1,60 @@
+package cn.chahuyun.economy.constant;
+
+/**
+ * @author Moyuyanli
+ * @date 2024/9/2 15:18
+ */
+public class EconPerm {
+
+    /**
+     * 钓鱼权限code
+     */
+    public final static String FISH_PERM = "fish";
+
+    /**
+     * 钓鱼权限组
+     */
+    public final static String FISH_PERM_GROUP = "钓鱼组";
+
+    /**
+     * 猜签权限
+     */
+    public final static String LOTTERY_PERM = "lottery";
+
+    /**
+     * 猜签权限组
+     */
+    public final static String LOTTERY_PERM_GROUP = "猜签组";
+
+    /**
+     * 抢劫权限
+     */
+    public final static String ROB_PERM = "rob";
+
+    /**
+     * 抢劫权限组
+     */
+    public final static String ROB_PERM_GROUP = "抢劫组";
+
+    /**
+     * 红包权限
+     */
+    public final static String RED_PACKET_PERM = "red-pack";
+
+    /**
+     * 红包权限组
+     */
+    public final static String RED_PACKET_PERM_GROUP = "红包组";
+
+    /**
+     * 签到黑名单权限
+     */
+    public final static String SIGN_BLACK_PERM = "sign-black";
+
+    /**
+     * 签到黑名单权限组
+     */
+    public final static String SIGN_BLACK_GROUP = "签到黑名单";
+
+
+}
