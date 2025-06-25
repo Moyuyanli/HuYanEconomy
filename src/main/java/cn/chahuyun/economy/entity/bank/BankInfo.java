fa289b8a6c0952d48c61b5b6ba665c353e941915
Moyuyanli <572490972@qq.com>
2025-06-26 01:42:18 +0800

    refactor(entity): 重构 BankInfo 类中的 randomInterest 方法
    -优化了 randomInterest 方法的逻辑，使其更符合利率生成的实际情况- 添加了方法注释，详细解释了利率生成的原理和目的
    - 调整了利率生成的概率分布，增加了低利率和高利率的区分度

--- src/main/java/cn/chahuyun/economy/entity/bank/BankInfo.java
+++ src/main/java/cn/chahuyun/economy/entity/bank/BankInfo.java
@@ -84,15 +84,28 @@ public class BankInfo {
         this.interest = randomInterest();
     }
 
+    /**
+     * 随机生成一个利率值
+     * <p>
+     * 此方法用于模拟生成一个随机的利率值，用于表示用户对某个项目的利率程度
+     * 利率值的生成遵循特定的概率分布，以模拟不同用户利率的多样性
+     *
+     * @return 随机生成的利率值，正数表示较高利率，负数或零表示较低利率
+     */
     public static Integer randomInterest() {
+        // 生成1到100之间的随机整数，用于决定利率值的范围
         int i = RandomUtil.randomInt(1, 101);
-        if (i <= 70) {
-            return RandomUtil.randomInt(1, 3);
-        } else if (i <= 99) {
+        // 当随机数大于等于99时，表示有较高的利率，生成3到5之间的随机整数作为利率值
+        if (i >= 99) {
             return RandomUtil.randomInt(3, 6);
+        } else if (i >= 70) {
+            // 当随机数大于等于70时，表示有一般利率，生成1到3之间的随机整数作为利率值
+            return RandomUtil.randomInt(1, 3);
         } else {
+            // 当随机数小于70时，表示利率较低，生成-3到1之间的随机整数作为利率值
             return RandomUtil.randomInt(-3, 1);
         }
     }
+
 
 }
