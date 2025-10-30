3c5d56a73ab25ca7c8fff9b61e6bc126112c1bc4
moyuyanli <572490972@qq.com>
2025-10-30 20:03:16 +0800

    feat(economy): 添加奖品类型定义与处理逻辑
    - 新增奖品类型枚举 PrizeType，包含普通、道具、称号、金钱和惩罚
    - 创建 PrizeHandle 单例对象用于处理不同类型的奖品- 实现金钱类奖品的兑换逻辑，支持从元数据中获取金额并增加到用户账户
    - 预留称号、道具和惩罚类奖品的处理方法
    - 添加异常处理机制确保兑奖过程的安全性

---
+++ src/main/java/cn/chahuyun/economy/constant/PrizeType.java
@@ -0,0 +1,24 @@
+package cn.chahuyun.economy.constant;
+
+public enum PrizeType {
+    /**
+     * 普通
+     */
+    ORDINARY,
+    /**
+     * 道具
+     */
+    PROP,
+    /**
+     * 称号
+     */
+    TITLE,
+    /**
+     * 钱
+     */
+    MONEY,
+    /**
+     * 惩罚
+     */
+    PUNISHMENT
+}
---
+++ src/main/kotlin/cn/chahuyun/economy/prizes/PrizeHandle.kt
@@ -0,0 +1,43 @@
+package cn.chahuyun.economy.prizes
+
+import cn.chahuyun.economy.constant.PrizeType
+import cn.chahuyun.economy.exception.RaffleException
+import cn.chahuyun.economy.utils.EconomyUtil
+import net.mamoe.mirai.Bot
+
+object PrizeHandle {
+
+    fun handle(prizes: List<RaffleResult>) {
+        prizes.forEach {
+            when (it.prize.type) {
+                PrizeType.ORDINARY -> return@forEach
+                PrizeType.MONEY -> handleMoney(it)
+                PrizeType.TITLE -> handleTitle(it)
+                PrizeType.PROP -> handleProp(it)
+                PrizeType.PUNISHMENT -> handlePunishment(it)
+            }
+        }
+    }
+
+
+    private fun handleMoney(raffleResult: RaffleResult) {
+        val prize = raffleResult.prize
+        val userId = raffleResult.userId
+        val groupId = raffleResult.groupId
+        val member = Bot.instances.first().getGroup(groupId)?.get(userId) ?: throw RaffleException("兑奖异常")
+        val money = prize.metadata["money"] ?: throw RaffleException("兑奖异常,奖金为空!")
+        EconomyUtil.plusMoneyToUser(member, money.toDouble())
+    }
+
+    private fun handleTitle(raffleResult: RaffleResult) {
+
+    }
+
+    private fun handleProp(raffleResult: RaffleResult) {
+
+    }
+
+    private fun handlePunishment(raffleResult: RaffleResult) {
+
+    }
+}
